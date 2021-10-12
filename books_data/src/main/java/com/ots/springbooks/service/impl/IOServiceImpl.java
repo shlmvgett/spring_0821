6ee221de2fea68641a6abc697af97ab34bc18e72
ots <your@project-specific-email.com>
2021-10-12 22:59:07 +0300

    L11: spring data book

---
+++ books_data/src/main/java/com/ots/springbooks/service/impl/IOServiceImpl.java
@@ -0,0 +1,21 @@
+package com.ots.springbooks.service.impl;
+
+import com.ots.springbooks.service.IOService;
+import java.util.Scanner;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+public class IOServiceImpl implements IOService {
+
+  @Override
+  public void print(String message) {
+    System.out.println(message);
+  }
+
+  @Override
+  public String read() {
+    return new Scanner(System.in).nextLine();
+  }
+}
